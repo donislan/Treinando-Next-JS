620f809c231b2a6fac146b4e561cb573bd39c587
Adonislan Alves <adonislan_alves@outlook.com>
2024-09-22 18:40:53 -0300

    trabalhando na tela do usuário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Header from "./Header";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Container, Button } from "reactstrap";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -10,14 +13,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="../public/favicon.ico" />
       </Head>
 
+      <Header />
+
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error
-          repellendus rem labore suscipit atque.
-        </p>
+        <Container className="py-5 text-center">
+          <h1 className="mt-5 display-1">
+            O melhor jeito de comprar o que você ama
+          </h1>
+          <p className="my-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum et
+            mollitia repellat facere iste culpa?
+          </p>
+          <Link href="/products">
+            <Button className="px-4 pb-2">Conheça nossos Produtos!</Button>
+          </Link>
+        </Container>
       </main>
     </>
   );
